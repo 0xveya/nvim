b26d853f3ffdc4933f6b855fb741f3749aeab6dd
0xveya <ghgstefan@gmail.com>
2026-04-12 22:38:06 +0200

    treesiter thingy fixed

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,8 @@
-vim.opt_local.conceallevel = 2
+-- Neovim 0.12 + markdown preview in snacks picker can crash in Tree-sitter's
+-- conceal path. Keep conceal for normal markdown buffers, but disable it in the
+-- picker preview window.
+if vim.w.snacks_picker_preview then
+	vim.opt_local.conceallevel = 0
+else
+	vim.opt_local.conceallevel = 2
+end
