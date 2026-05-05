e89b9e1d8bb3082a35bbaa67595de20d5fc06dfe
0xveya <ghgstefan@gmail.com>
2026-05-05 21:56:24 +0200

    Add go mono repo plugin

---
+++ lua/plugins/go-mono-repo.lua
@@ -0,0 +1,13 @@
+return {
+	"0xveya/go-mono-repo",
+	opts = {
+		persist = true,
+		override = {
+			enabled = true,
+			files = "<leader>ff",
+			grep = "<leader>fg",
+			symbols = "<leader>fs",
+			handlers = "<leader>fh",
+		},
+	},
+}
