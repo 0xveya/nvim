34982b543ee834c1d28a938779249c0d44fd62de
0xveya <ghgstefan@gmail.com>
2026-05-05 23:03:28 +0200

    Show Go mono scope in statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,38 @@ return {
 		config = function()
 			require("mini.ai").setup({ n_lines = 500 })
 			require("mini.surround").setup()
-			require("mini.statusline").setup()
+			local statusline = require("mini.statusline")
+			statusline.setup({
+				content = {
+					active = function()
+						local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+						local git = statusline.section_git({ trunc_width = 40 })
+						local diff = statusline.section_diff({ trunc_width = 75 })
+						local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+						local lsp = statusline.section_lsp({ trunc_width = 75 })
+						local filename = statusline.section_filename({ trunc_width = 140 })
+						local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+						local location = statusline.section_location({ trunc_width = 75 })
+						local search = statusline.section_searchcount({ trunc_width = 75 })
+						local go_scope = ""
+
+						local ok, gomono = pcall(require, "go_mono_repo")
+						if ok then
+							go_scope = gomono.status()
+						end
+
+						return statusline.combine_groups({
+							{ hl = mode_hl, strings = { mode } },
+							{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp, go_scope } },
+							"%<",
+							{ hl = "MiniStatuslineFilename", strings = { filename } },
+							"%=",
+							{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+							{ hl = mode_hl, strings = { search, location } },
+						})
+					end,
+				},
+			})
 		end,
 	},
 }
