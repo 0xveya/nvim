6f5d100646793eb825a77b9bd878e09dd9adf32b
0xveya <94190179+0xveya@users.noreply.github.com>
2026-08-29 03:10:42 +0200

    deine mama ist dein mama

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,4 +1,6 @@
 local function apply_highlights()
+	vim.api.nvim_set_hl(0, "Type", { fg = "#c4a7e7" })
+	vim.api.nvim_set_hl(0, "@type", { fg = "#c4a7e7" })
 	vim.api.nvim_set_hl(0, "@function", { fg = "#ebbcba" })
 	vim.api.nvim_set_hl(0, "@constant.macro", { fg = "#d47a9f" })
 	vim.api.nvim_set_hl(0, "@function.macro", { fg = "#d47a9f" })
